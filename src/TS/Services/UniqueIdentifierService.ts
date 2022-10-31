4c8bd6171efefb1461f860075167fd6193f261d2
Damian Connolly <16608228+Damo1155@users.noreply.github.com>
2022-10-31 16:30:41 +0000

    Redundant services removed.

--- src/TS/Services/UniqueIdentifierService.ts
+++
@@ -1,3 +0,0 @@
-export const GenerateIdentifier = (): number => {
-    return parseInt(Math.ceil(Math.random() * Date.now()).toPrecision(16).toString().replace(".", ""));
-}
--- src/TS/searchboxToRemove.ts
+++ src/TS/searchboxToRemove.ts
@@ -6,8 +6,6 @@ import { SearchBoxGroups, SearchBoxOptions } from "./Types/ListBoxItem";
 
 // Services
 import { ConfigureMarkup } from "Services/MarkupService";
-import { GenerateIdentifier } from "Services/UniqueIdentifierService";
-import { RemoveItem, SetJSONObject } from "Services/SessionManagementService";
 
 // Listeners
 import { DestroySearchListeners } from "Listeners/SearchContainerListeners";
@@ -15,36 +13,36 @@ import { InitialiseExpanderListeners, DestroyExpanderListeners } from "Listeners
 import { InitialiseOptionListeners, DestroySelectableOptionsListeners } from "Listeners/SelectableOptionsListeners";
 
 export const initialise = (): void => {
-    const elements = document.getElementsByClassName("searchbox");
+    // const elements = document.getElementsByClassName("searchbox");
 
-    // TODO :   On key down or up navigate the tree of 'options'
-    for (const element of elements) {
-        const uuid = GenerateIdentifier();
+    // // TODO :   On key down or up navigate the tree of 'options'
+    // for (const element of elements) {
+    //     const uuid = GenerateIdentifier();
 
-        element.innerHTML = ConfigureMarkup(element, uuid);
+    //     element.innerHTML = ConfigureMarkup(element, uuid);
 
-        InitialiseExpanderListeners(element, uuid);
-    }
+    //     InitialiseExpanderListeners(element, uuid);
+    // }
 
     // TODO :   Before closing the tab, refresh or redirect wipe out the session storeage
 }
 
 export const createContainer = (element: HTMLElement, options?: Array<SearchBoxGroups | SearchBoxOptions>): void => {
-    const uuid = GenerateIdentifier();
+    // const uuid = GenerateIdentifier();
 
-    element.innerHTML = ConfigureMarkup(element, uuid);
+    // element.innerHTML = ConfigureMarkup(element, uuid);
 
-    InitialiseExpanderListeners(element, uuid);
-    InitialiseOptionListeners(element, uuid);
+    // InitialiseExpanderListeners(element, uuid);
+    // InitialiseOptionListeners(element, uuid);
 }
 
 export const updateOptions = (element: HTMLElement, options: Array<SearchBoxGroups | SearchBoxOptions>): void => {
     // TODO :   If selected options is no longer available after the update then clear the sessionStorage object and reset the search box
 
-    const firstChildElement = element.querySelector("div"),
-        searchBoxUuid = parseInt(firstChildElement.getAttribute("data-selectbox-id"));
+    // const firstChildElement = element.querySelector("div"),
+    //     searchBoxUuid = parseInt(firstChildElement.getAttribute("data-selectbox-id"));
 
-    SetJSONObject(`${CONSTANTS.SessionStorageOptionsPrefix}-${searchBoxUuid}`, options);
+    // SetJSONObject(`${CONSTANTS.SessionStorageOptionsPrefix}-${searchBoxUuid}`, options);
 }
 
 export const addAdditionalOptions = (element: HTMLElement, options: Array<SearchBoxGroups> | Array<SearchBoxOptions>): void => {
@@ -52,31 +50,31 @@ export const addAdditionalOptions = (element: HTMLElement, options: Array<Search
 }
 
 export const destroy = (element: HTMLElement): void => {
-    const searchBoxUuid = element.querySelector("div").getAttribute("data-selectbox-id");
+    // const searchBoxUuid = element.querySelector("div").getAttribute("data-selectbox-id");
 
-    RemoveItem(`${CONSTANTS.SessionStorageValuePrefix}-${searchBoxUuid}`);
-    RemoveItem(`${CONSTANTS.SessionStorageOptionsPrefix}-${searchBoxUuid}`);
+    // RemoveItem(`${CONSTANTS.SessionStorageValuePrefix}-${searchBoxUuid}`);
+    // RemoveItem(`${CONSTANTS.SessionStorageOptionsPrefix}-${searchBoxUuid}`);
 
-    DestroySearchListeners(element);
-    DestroyExpanderListeners(element);
-    DestroySelectableOptionsListeners(element);
+    // DestroySearchListeners(element);
+    // DestroyExpanderListeners(element);
+    // DestroySelectableOptionsListeners(element);
 
-    element.remove();
+    // element.remove();
 }
 
 export const destroyAll = (): void => {
-    const container = document.getElementsByClassName("searchbox");
+    // const container = document.getElementsByClassName("searchbox");
 
-    for (const element of container) {
-        const searchBoxUuid = element.querySelector("div").getAttribute("data-searchbox-id");
+    // for (const element of container) {
+    //     const searchBoxUuid = element.querySelector("div").getAttribute("data-searchbox-id");
 
-        RemoveItem(`${CONSTANTS.SessionStorageValuePrefix}-${searchBoxUuid}`);
-        RemoveItem(`${CONSTANTS.SessionStorageOptionsPrefix}-${searchBoxUuid}`);
+    //     RemoveItem(`${CONSTANTS.SessionStorageValuePrefix}-${searchBoxUuid}`);
+    //     RemoveItem(`${CONSTANTS.SessionStorageOptionsPrefix}-${searchBoxUuid}`);
         
-        DestroySearchListeners(element);
-        DestroyExpanderListeners(element);
-        DestroySelectableOptionsListeners(element);
+    //     DestroySearchListeners(element);
+    //     DestroyExpanderListeners(element);
+    //     DestroySelectableOptionsListeners(element);
 
-        element.remove();
-    }
+    //     element.remove();
+    // }
 }
